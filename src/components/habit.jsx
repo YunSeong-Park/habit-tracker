34c5817f18bf6c9923f61ab2a6182d394dc42258
YunSeong Park <75507166+kairase024@users.noreply.github.com>
2021-03-22 19:55:41 +0900

    create habits

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -16,14 +16,21 @@ class Habit extends Component {
   };
   
   render() {
+    const { name, count } = this.props.habit; 
     return (
       <li className="habit">
-        <span className="habit-name">Reading</span>
-        <span className="habit-count">{this.state.count}</span>
-        <button className="habit-button habit-increase" onClick= {this.handleIncrement}>
+        <span className="habit-name">{name}</span>
+        <span className="habit-count">{count}</span>
+        <button
+          className="habit-button habit-increase"
+          onClick={this.handleIncrement}
+        >
           <i className="fas fa-plus-square"></i>
         </button>
-        <button className="habit-button habit-decrease" onClick= {this.handleDecreament}>
+        <button
+          className="habit-button habit-decrease"
+          onClick={this.handleDecreament}
+        >
           <i className="fas fa-minus-square"></i>
         </button>
         <button className="habit-button habit-delete">
