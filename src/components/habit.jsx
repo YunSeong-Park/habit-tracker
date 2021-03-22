20bc31f604394eb1ce2a04958d40b586f6d490f2
YunSeong Park <75507166+kairase024@users.noreply.github.com>
2021-03-22 19:36:55 +0900

    create habbit component

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,11 +1,35 @@
 import React, { Component } from "react";
 
 class Habit extends Component {
+  state = {
+    count: 0,
+  };
+
+  handleIncrement = () => {
+    // state 오브젝트 안에 있는 count를 증가 한뒤 state를 업데이트 해야함. 
+    this.setState({count: this.state.count + 1});
+  };
+  
+  handleDecreament = () => {
+    const count = this.state.count -1;
+    this.setState({count: count < 0 ? 0 : this.state.count - 1});
+  };
+  
   render() {
     return (
-      <>
-        <span className="habit-name">Reading</span>;
-      </>
+      <li className="habit">
+        <span className="habit-name">Reading</span>
+        <span className="habit-count">{this.state.count}</span>
+        <button className="habit-button habit-increase" onClick= {this.handleIncrement}>
+          <i className="fas fa-plus-square"></i>
+        </button>
+        <button className="habit-button habit-decrease" onClick= {this.handleDecreament}>
+          <i className="fas fa-minus-square"></i>
+        </button>
+        <button className="habit-button habit-delete">
+          <i className="fas fa-trash"></i>
+        </button>
+      </li>
     );
   }
 }
