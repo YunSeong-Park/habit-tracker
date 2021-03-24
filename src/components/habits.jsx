eaffcaac4a1b5f1d88eac198b23094dded42c86c
YunSeong Park <75507166+kairase024@users.noreply.github.com>
2021-03-24 09:01:05 +0900

    add props

--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -11,15 +11,25 @@ class Habits extends Component {
   };
 
   handleIncrement = habit => {
-    this.setState({count: this.state.})
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({ habits });
   };
 
   handleDecrement = habit => {
-    console.log(`handleDcrement ${habit.name}`);
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    const count = habits[index].count - 1;
+    habits[index].count = count < 0 ? 0 : count;
+
+    this.setState({ habits });
   };
 
   handleDelete = habit => {
     console.log(`handleDelete ${habit.name}`);
+    habits.splice(index, 1);
+    this.setState({ habits });
   };
 
   render() {
